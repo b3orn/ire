5df950a12781dffe22701e30f8a167eef907ad95
Björn Schulz <bjoern@voidiac.com>
2014-10-20 23:06:53 +0200

    implemented ire_cpu_t initialisation

--- src/cpu.c
+++ src/cpu.c
@@ -3,11 +3,43 @@
 
 IRE_API(ire_error_t)
 ire_cpu_create(ire_binary_t *binary, ire_cpu_t **destination) {
-    if (binary == NULL || destination == NULL) {
+    ire_cpu_t *self;
+
+    if (binary == NULL || binary->data == NULL || binary->code == NULL || destination == NULL) {
         return IRE_ERROR_NULL_POINTER;
     }
 
-    return IRE_ERROR_NOT_IMPLEMENTED;
+    self = malloc(sizeof(*self) + 0x10000);
+
+    if (self == NULL) {
+        return IRE_ERROR_OUT_OF_MEMORY;
+    }
+
+    self->registers[IRE_RIP] = binary->code->entry;
+    self->registers[IRE_RSP] = binary->code->stack_base;
+
+    if (binary->data->zero > 0) {
+        memset(&(self->memory[binary->data->base]), 0, binary->data->zero);
+    }
+
+    memcpy(
+        &(self->memory[binary->data->base + binary->data->zero]),
+        binary->data->data,
+        binary->data->size);
+
+    memcpy(
+        &(self->memory[binary->code->interrupt_base]),
+        binary->code->interrupt_table,
+        sizeof(binary->code->interrupt_table));
+
+    memcpy(
+        &(self->memory[binary->code->base]),
+        binary->code->data,
+        binary->code->size);
+
+    *destination = self;
+
+    return IRE_OK;
 }
 
 
@@ -17,5 +49,9 @@ ire_cpu_destroy(ire_cpu_t **self) {
         return IRE_ERROR_NULL_POINTER;
     }
 
-    return IRE_ERROR_NOT_IMPLEMENTED;
+    free(*self);
+
+    *self = NULL;
+
+    return IRE_OK;
 }
