d1a9f8e05a6f40979fb468a9dd6f26f5b55c33aa
Björn Schulz <bjoern@voidiac.com>
2014-10-20 22:16:56 +0200

    reordered parameters of ire_binary_create

--- include/ire/binary.h
+++ include/ire/binary.h
@@ -2,8 +2,8 @@
 #define IRE_BINARY_H
 
 IRE_API(ire_error_t) ire_binary_create(
-    ire_code_t *code,
     ire_data_t *data,
+    ire_code_t *code,
     ire_binary_t **destination);
 
 
--- src/binary.c
+++ src/binary.c
@@ -2,9 +2,9 @@
 
 
 IRE_API(ire_error_t)
-ire_binary_create(ire_code_t *code, ire_data_t *data,
+ire_binary_create(ire_data_t *data, ire_code_t *code,
                   ire_binary_t **destination) {
-    if (code == NULL || data == NULL || destination == NULL) {
+    if (data == NULL || code == NULL || destination == NULL) {
         return IRE_ERROR_NULL_POINTER;
     }
 
